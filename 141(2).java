e96179d73a4e48b87f7420415cc1bdcd576fe65f
Manava <114528094+manava10@users.noreply.github.com>
2024-10-03 09:19:11 +0530

    Create 141(2).java
    //

---
+++ 141(2).java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) { 
+        //This can be done by HashMap. But optimal solution can only be
+        //Achieved through Tortoies Hare method
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow==fast){
+                return true;
+            }
+        }
+        return false;   
+    }
+}
+//HareTortoise Method
